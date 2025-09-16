b23a7e2db8c3007d61d03dd61b3aae8816e2d154
motumbon <pabloyevenes.qf@gmail.com>
2025-09-16 00:02:55 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs').promises;
 const path = require('path');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const app = express();
